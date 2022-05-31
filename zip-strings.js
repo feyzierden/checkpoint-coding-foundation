7bb767f4f91df0bf1c9234415dadb48904363401
Feyzi Erden <feyzi.erden2@gmail.com>
2022-05-31 10:20:46 +0200

    solved the 3rd Kata

--- zip-strings.js
+++ zip-strings.js
@@ -1,5 +1,22 @@
 function zipStrings(strA, strB) {
-  console.log("test");
+  let maxLength = 0;
+  let result = "";
+
+  strA.length > strB.length
+    ? (maxLength = strA.length)
+    : (maxLength = strB.length);
+
+  for (i = 0; i < maxLength; i++) {
+    if (strA[i] !== undefined) {
+      result += strA[i];
+    }
+
+    if (strB[i] !== undefined) {
+      result += strB[i];
+    }
+  }
+
+  return result;
 }
 
-zipStrings();
+// In JSBin funktionieren alle Testfälle. Hier wird der Testfall "a" "b" als Fehler dargestellt. Warum?
